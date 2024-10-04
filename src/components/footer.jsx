eb3b30b1ea5c48e95150dd9ed25dff81f2f4cbb3
vishwajeet1729 <vishwajeetwalse9767@gmail.com>
2024-10-04 20:36:33 +0530

    pushing

---
+++ src/components/footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Footer Component
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                Created with <i className="fa fa-heart" style={{color: "#FF5252"}}></i> by
+                <a href="3" target="_blank" rel="noopener noreferrer" className="link">
+                    <span> </span>
+                 vishwajeet Walse
+                </a>
+            </p>
+        </footer>
+    );
+}
+ 
+export default Footer;
